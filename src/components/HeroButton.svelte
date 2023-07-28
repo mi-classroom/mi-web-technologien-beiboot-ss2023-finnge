166e54f93b67158b7204047bbe7c56e74bde039e
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-07-28 16:15:25 +0200

    basic style and button

---
+++ src/components/HeroButton.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  export let type: 'primary' | 'secondary' | 'tertiary' = 'primary';
+
+  export let icon: string = '';
+
+  // bubble some Anchor attributes
+  export let href: HTMLAnchorElement['href'] = '';
+  export let target: '_self' | '_blank' | HTMLAnchorElement['target'] = '';
+</script>
+
+<a {href} {target} data-type={type}>
+  {#if icon}
+    <span class="icon">{icon}</span>
+  {/if}
+  <slot />
+</a>
+
+<style lang="scss">
+  a {
+    display: inline-block;
+    padding: var(--spacing-sm) var(--spacing-md);
+    border-radius: var(--border-radius-sm);
+    text-decoration: none;
+    color: var(--color-lightest);
+    font-size: var(--fs-xl);
+    line-height: var(--lh-xs);
+    text-align: center;
+  }
+
+  a[data-type='primary'] {
+    background-color: var(--color-primary);
+
+    &:hover, &:focus {
+      background-color: var(--color-primary-dark);
+    }
+  }
+
+  a[data-type='secondary'] {
+    background-color: var(--color-darkest);
+
+    &:hover, &:focus {
+      background-color: var(--color-darker);
+    }
+  }
+
+  a[data-type="tertiary"] {
+    background-color: var(--color-light);
+    color: var(--color-darkest);
+
+    &:hover, &:focus {
+      background-color: var(--color-lighter);
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,28 @@
-<h1>ARlebnispfade Oberberg</h1>
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  import HeroButton from '../components/HeroButton.svelte';
+  
+  export let data: PageData;
+</script>
+
+<section>
+  <h1>ARlebnispfade Oberberg</h1>
+  <h2>Oberbergischer Kreis</h2>
+
+  <video></video>
+</section>
+
+<section>
+
+  <HeroButton type="primary" href="/pfad">
+    <span class="fs-md">Ich will nur ein bisschen</span><br>
+    stöbern
+  </HeroButton>
+  <HeroButton type="secondary" href="/start">
+    <span class="fs-md">Ich will die ARlebnisse</span><br>
+    nutzen
+  </HeroButton>
+
+</section>
+
