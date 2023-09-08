17a435a1e158a840427ddec2d2a5341dd533ed6a
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-09-08 23:15:19 +0200

    docs: add math description

--- src/utils/geoCoords.ts
+++ src/utils/geoCoords.ts
@@ -12,6 +12,18 @@ const EARTH_RADIUS = 6371e3;
  * Returns the distance between two points in metres.
  * @see https://www.movable-type.co.uk/scripts/latlong.html
  *
+ * @description
+ * This uses the haversine formula to calculate the distance
+ * between two points.
+ *  a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+ *  c = 2 ⋅ atan2( √a, √(1−a) )
+ *  d = R ⋅ c
+ * where
+ *  φ is latitude,
+ *  λ is longitude,
+ *  R is earth’s radius (mean radius = 6,371km);
+ * note that angles need to be in radians to pass to trig functions!
+ *
  * @param {GeoPoint} point1
  * @param {GeoPoint} point2
  * @returns {number} distance in metres
