1f3c0b66b3094eedf3c08c127213bbbd16bf76ab
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-09-16 13:18:18 +0200

    Feat: First AR in Svelte Context (#17)
    * implement webxr to svelte
    
    * add debug
    
    * add transparency to build
    
    * make page dynamic
    
    * add distance to arlebnis
    
    * show ar in dom

--- src/routes/arlebnis/[id]/+page.server.ts
+++ src/routes/arlebnis/[id]/+page.server.ts
@@ -5,6 +5,5 @@ import { data as arExperienceData } from "@data/arExperiences";
 export const entries = (() => {
   return arExperienceData.map((arExperience) => ({
     id: arExperience.id,
-    data: arExperience,
   }));
 }) satisfies EntryGenerator;
