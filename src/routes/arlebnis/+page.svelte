3187ab48bd69c4f55778e2b539f34a6f1d728661
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-09-19 14:36:29 +0200

    feat: add notice for qr scanner

--- src/routes/arlebnis/+page.svelte
+++ src/routes/arlebnis/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-  import TopBar from "../../components/TopBar.svelte";
+  import TopBar from "@/components/TopBar.svelte";
 </script>
 
 <TopBar title="ARlebnis via QR Code starten" />
+
+<p>
+  Der QR-Scanner ist nicht implementiert. Nutze bitte den Geräte-eigenen QR-Scanner.
+</p>
